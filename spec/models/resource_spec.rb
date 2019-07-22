8315178b5d5d08a8c475237ce8dac97d9e17c801
Emily Giurleo <e.m.giurleo@gmail.com>
2019-07-22 14:49:09 -0400

    fix resource model spec

--- spec/models/resource_spec.rb
+++ spec/models/resource_spec.rb
@@ -4,22 +4,29 @@ RSpec.describe Resource, type: :model do
   describe '#valid?' do
     context 'when state code is invalid' do
       it 'returns false' do
-        resource = build(:resource, state: 'XO')
+        resource = build(:resource, :with_resource_category, state: 'XO')
         expect(resource.valid?).to be false
       end
     end
     
     context 'when state code is valid' do
       it 'returns true' do
-        resource = build(:resource, state: 'NY')
+        resource = build(:resource, :with_resource_category, state: 'NY')
         expect(resource.valid?).to be true
       end
     end
 
     context 'when state code is nil' do
       it 'returns true' do
-        resource = build(:resource)
+        resource = build(:resource, :with_resource_category)
         expect(resource.valid?).to be true
+      end
+    end
+
+    context 'with no resource category' do
+      it 'returns false' do
+        resource = build(:resource, state: 'NY')
+        expect(resource.valid?).to be false
       end
     end
   end
