99b4b1950005dcdb44a759113b41b7f005f58f5a
Emily Giurleo <e.m.giurleo@gmail.com>
2019-07-22 09:03:28 -0400

    validate state codes

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,3 +1,10 @@
+require 'carmen'
+
 class Resource < ApplicationRecord
+    include Carmen
+
+    STATE_CODES = Country.named('United States').subregions.map(&:code)
     # belongs_to :resource_category
+
+    validates :state, inclusion: { in: STATE_CODES, message: "%{value} is not a valid US state code" }
 end
--- spec/models/resource_spec.rb
+++ spec/models/resource_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Resource, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#valid?' do
+    context 'when state code is invalid' do
+      it 'returns false' do
+        resource = build(:resource, state: 'XO')
+        expect(resource.valid?).to be false
+      end
+    end
+    
+    context 'when state code is valid' do
+      it 'returns true' do
+        resource = build(:resource, state: 'NY')
+        expect(resource.valid?).to be true
+      end
+    end
+  end
 end
