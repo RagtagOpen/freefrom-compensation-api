23b959a1674a1067497556e48623ead7165904e8
Emily Giurleo <e.m.giurleo@gmail.com>
2019-07-26 14:59:14 -0400

    add authentication to resource step controller spec

--- spec/controllers/resource_steps_controller_spec.rb
+++ spec/controllers/resource_steps_controller_spec.rb
@@ -2,9 +2,12 @@ require 'rails_helper'
 require 'spec_helper'
 
 describe ResourceStepsController, type: :controller do
-  describe '#show' do
-    let(:id) { 1000 }
+  let(:token) { Knock::AuthToken.new(payload: { sub: user.id }).token }
+  let(:headers) { { 'Authorization': "Bearer #{token}" } }
 
+  let(:id) { 1000 }
+
+  describe '#show' do
     context 'where resource step exists' do
       before do
         resource_step = create(:resource_step, :with_resource, id: id)
@@ -34,125 +37,202 @@ describe ResourceStepsController, type: :controller do
 
     let(:params) { { resource_id: resource_id, number: number } }
 
-    context 'an invalid resource id' do
-      let(:resource_id) { 'fake-id' }
-
-      it 'returns 400 and an error' do
-        post :create, params: params
-        expect(response.status).to eq(400)
-        
-        body = JSON.parse(response.body)
-        expect(body['error']).to eq("Validation failed: Resource must exist")
+    context 'without authentication' do
+      it 'returns 401' do
+        post :create, params: { resource_id: '123' }
+        expect(response.status).to eq(401)
       end
     end
 
-    context 'without number' do
-      let(:number) { nil }
+    context 'with regular user' do
+      let(:user) { create(:user) }
 
-      it 'returns 400 and an error' do
-        post :create, params: params
-        expect(response.status).to eq(400)
-        
-        body = JSON.parse(response.body)
-        expect(body['error']).to eq("Missing number parameter")
+      it 'returns 401' do
+        post :create, params: { resource_id: '123' }
+        expect(response.status).to eq(401)
       end
     end
 
-    it 'returns 201 and the resource step' do
-      post :create, params: params
-      expect(response.status).to eq(201)
+    context 'with admin user' do
+      let(:user) { create(:user, :admin) }
 
-      body = JSON.parse(response.body)
-      expect(body['id']).to be_a(Integer)
-      expect(body['resource_id']).to eq(resource_id)
-      expect(body['number']).to eq(number)
+      before do
+        request.headers.merge! headers
+      end
+
+      context 'an invalid resource id' do
+        let(:resource_id) { 'fake-id' }
+
+        it 'returns 400 and an error' do
+          post :create, params: params
+          expect(response.status).to eq(400)
+          
+          body = JSON.parse(response.body)
+          expect(body['error']).to eq("Validation failed: Resource must exist")
+        end
+      end
+
+      context 'without number' do
+        let(:number) { nil }
+
+        it 'returns 400 and an error' do
+          post :create, params: params
+          expect(response.status).to eq(400)
+          
+          body = JSON.parse(response.body)
+          expect(body['error']).to eq("Missing number parameter")
+        end
+      end
+
+      it 'returns 201 and the resource step' do
+        post :create, params: params
+        expect(response.status).to eq(201)
+
+        body = JSON.parse(response.body)
+        expect(body['id']).to be_a(Integer)
+        expect(body['resource_id']).to eq(resource_id)
+        expect(body['number']).to eq(number)
+      end
     end
   end
 
   describe '#destroy' do
-    let(:id) { 1000 }
 
     before do
       resource_step = create(:resource_step, :with_resource, id: id)
     end
 
-    it 'returns 204 and an empty body' do
-      delete :destroy, params: { id: id }
-      expect(response.status).to eq(204)
-
-      body = JSON.parse(response.body)
-      expect(body).to be_empty
+    context 'without authentication' do
+      it 'returns 401' do
+        delete :destroy, params: { id: id }
+        expect(response.status).to eq(401)
+      end
     end
-  end
 
-  describe '#update' do
-    let(:id) { 1000 }
+    context 'with regular user' do
+      let(:user) { create(:user) }
 
-    context 'where resource step doesn\'t exist' do
-      it 'returns 404 and an empty body' do
-        put :update, params: { id: id }
-        expect(response.status).to eq(404)
+      before do
+        request.headers.merge! headers
+      end
+
+      it 'returns 401' do
+        delete :destroy, params: { id: id }
+        expect(response.status).to eq(401)
+      end
+    end
+
+    context 'with admin user' do
+      let(:user) { create(:user, :admin) }
+
+      before do
+        request.headers.merge! headers
+      end
+
+      it 'returns 204 and an empty body' do
+        delete :destroy, params: { id: id }
+        expect(response.status).to eq(204)
 
         body = JSON.parse(response.body)
         expect(body).to be_empty
       end
     end
+  end
 
-    context 'where resource step does exist' do
-      let!(:resource_step) { create(:resource_step, :with_resource, id: id) }
-      let!(:new_resource) { create(:resource, :with_resource_category) }
+  describe '#update' do
+    context 'with no authentication' do
+      it 'returns 401' do
+        put :update, params: { id: id }
+        expect(response.status).to eq(401)
+      end
+    end
 
-      context 'and update succeeds' do
-        let(:params) do
-          {
-            description: "resource step description",
-            number: 1,
-            resource_id: new_resource.id.to_i,
-          }
-        end
+    context 'with regular user' do
+      let(:user) { create(:user) }
+      
+      before do
+        request.headers.merge! headers
+      end
 
-        it 'returns 200 and new resource' do
-          put :update, params: params.merge({ id: id })
-          expect(response.status).to eq(200)
+      it 'returns 401' do
+        put :update, params: { id: id }
+        expect(response.status).to eq(401)
+      end
+    end
+
+    context 'with admin user' do
+      let(:user) { create(:user, :admin) }
+
+      before do
+        request.headers.merge! headers
+      end
+
+      context 'where resource step doesn\'t exist' do
+        it 'returns 404 and an empty body' do
+          put :update, params: { id: id }
+          expect(response.status).to eq(404)
 
           body = JSON.parse(response.body)
-          keys = %w(description number resource_id)
-
-          keys.each do |key|
-            expect(body[key]).to eq(params[key.to_sym])
-          end
+          expect(body).to be_empty
         end
+      end
 
-        context 'only altering one field' do
-          let(:new_description) { 'New resource step description' }
-          let(:old_number) { 1 }
-          let(:params) { { description: new_description } }
+      context 'where resource step does exist' do
+        let!(:resource_step) { create(:resource_step, :with_resource, id: id) }
+        let!(:new_resource) { create(:resource, :with_resource_category) }
 
-          before do
-            resource_step.number = old_number
-            resource_step.save!
+        context 'and update succeeds' do
+          let(:params) do
+            {
+              description: "resource step description",
+              number: 1,
+              resource_id: new_resource.id.to_i,
+            }
           end
 
-          it 'updates the fields passed into the params and does not modify anything else' do
+          it 'returns 200 and new resource' do
             put :update, params: params.merge({ id: id })
             expect(response.status).to eq(200)
 
             body = JSON.parse(response.body)
-            expect(body['description']).to eq(new_description)
-            expect(body['number']).to eq(old_number)
+            keys = %w(description number resource_id)
+
+            keys.each do |key|
+              expect(body[key]).to eq(params[key.to_sym])
+            end
           end
-        end
 
-        context 'with an invalid field' do
-          let(:new_number) { nil }
-          let(:params) { { number: new_number } }
+          context 'only altering one field' do
+            let(:new_description) { 'New resource step description' }
+            let(:old_number) { 1 }
+            let(:params) { { description: new_description } }
 
-          it 'returns 400 and an error message' do
-            put :update, params: params.merge({ id: id })
-            expect(response.status).to eq(400)
-            
-            body = JSON.parse(response.body)
-            expect(body['error']).to eq("Validation failed: Number can't be blank")
+            before do
+              resource_step.number = old_number
+              resource_step.save!
+            end
+
+            it 'updates the fields passed into the params and does not modify anything else' do
+              put :update, params: params.merge({ id: id })
+              expect(response.status).to eq(200)
+
+              body = JSON.parse(response.body)
+              expect(body['description']).to eq(new_description)
+              expect(body['number']).to eq(old_number)
+            end
+          end
+
+          context 'with an invalid field' do
+            let(:new_number) { nil }
+            let(:params) { { number: new_number } }
+
+            it 'returns 400 and an error message' do
+              put :update, params: params.merge({ id: id })
+              expect(response.status).to eq(400)
+              
+              body = JSON.parse(response.body)
+              expect(body['error']).to eq("Validation failed: Number can't be blank")
+            end
           end
         end
       end
