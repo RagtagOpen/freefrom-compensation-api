8e98e37fe081e2379384265bfb03d42fdbe18b6e
Emily Giurleo <e.m.giurleo@gmail.com>
2019-09-14 14:35:40 -0400

    clean up resources controller spec

--- spec/controllers/resources_controller_spec.rb
+++ spec/controllers/resources_controller_spec.rb
@@ -3,26 +3,18 @@ require_relative './shared/unauthenticated_spec'
 require_relative './shared/regular_user_spec'
 
 describe ResourcesController, type: :controller do
-  let(:token) { Knock::AuthToken.new(payload: { sub: user.id }).token }
-  let(:headers) { { 'Authorization': "Bearer #{token}" } }
-
-  let(:id) { 1000 }
-
   it_behaves_like 'an unauthenticated object', Resource, {
     create: { resource_category_id: 123 }
   }
+
   it_behaves_like 'an object authenticated with a regular user', Resource, {
     create: { resource_category_id: 123 }
   }
 
-  describe '#create' do
-    context 'with admin user' do
-      let(:user) { create(:user, :admin) }
+  context 'with admin user' do
+    setup_admin_controller_spec
 
-      before do
-        request.headers.merge! headers
-      end
-
+    describe '#create' do
       let!(:resource_category) { create(:resource_category) }
       let(:resource_category_id) { resource_category.id.to_i }
       let(:state) { 'NY' }
@@ -61,18 +53,10 @@ describe ResourcesController, type: :controller do
         expect(body['id']).to be_a(Integer)
       end
     end
-  end
 
-  describe '#destroy' do
-    before do
-      create(:resource, :with_resource_category, id: id)
-    end
-
-    context 'with admin user' do
-      let(:user) { create(:user, :admin) }
-
+    describe '#destroy' do
       before do
-        request.headers.merge! headers
+        create(:resource, :with_resource_category, id: id)
       end
 
       it 'returns 204 and an empty body' do
@@ -83,16 +67,8 @@ describe ResourcesController, type: :controller do
         expect(body).to be_empty
       end
     end
-  end
 
-  describe '#update' do
-    context 'with admin user' do
-      let(:user) { create(:user, :admin) }
-      
-      before do
-        request.headers.merge! headers
-      end
-
+    describe '#update' do
       context 'where resource doesn\'t exist' do
         it 'returns 404 and an empty body' do
           put :update, params: { id: id }
