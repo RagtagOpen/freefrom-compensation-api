14c4e5dc0efb6fd88443d44601dd61f374e59386
Emily Giurleo <e.m.giurleo@gmail.com>
2019-11-13 01:05:16 -0500

    simplify model even further

--- db/migrate/20191109222528_change_resource.rb
+++ db/migrate/20191109222528_change_resource.rb
@@ -2,8 +2,8 @@ class ChangeResource < ActiveRecord::Migration[5.2]
   def change
     change_table :resources do |t|
       t.text  :where
-      t.text  :what_to_expect
-      t.text  :what_if_i_disagree
+      t.text  :what_to_expect, default: [], array: true
+      t.text  :what_if_i_disagree, default: [], array: true
       t.text  :resources, default: [], array: true
       t.text  :steps, default: [], array: true
     end
