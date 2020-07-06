a05f0cdbeb88bd7d2871a96968fbe70dcf29b601
Emily Giurleo <e.m.giurleo@gmail.com>
2020-07-06 13:12:17 -0400

    fix array fields

--- app/controllers/admin/resources_controller.rb
+++ app/controllers/admin/resources_controller.rb
@@ -1,5 +1,27 @@
 # frozen_string_literal: true
 
 module Admin
-  class ResourcesController < Admin::ApplicationController; end
+  class ResourcesController < Admin::ApplicationController
+    def resource_params
+      array_fields = [
+        "tips",
+        "resources",
+        "steps",
+        "challenges",
+        "what_to_expect",
+        "what_if_i_disagree",
+      ]
+
+      array_fields.each do |field|
+        params["resource"][field] = params["resource"][field].split("\r\n\r\n*-*-*-*-*\r\n\r\n")
+      end
+
+      permitted_fields = array_fields.inject({}) do |h, field|
+        h[field.to_sym] = []
+        h
+      end
+
+      params.require(resource_name).permit(*dashboard.permitted_attributes, **permitted_fields)
+    end
+  end
 end
---
+++ app/fields/string_array_field.rb
@@ -0,0 +1,3 @@
+require "administrate/field/base"
+
+class StringArrayField < Administrate::Field::Base; end
